3bbc7877015579665d966452bf8feb111897a9ad
Kristina Pupavac <kristina.pupavac@bitcamp.ba>
2015-05-24 13:29:05 +0200

    Ovo je moja prva zadaca na git-u

---
+++ Task1.java
@@ -0,0 +1,20 @@
+
+public class Task1 {
+//Napisati program koji sadrzi tri cijela broja a, b i c. Ispisati da li je moguce
+//konstruisati trougao sa stranicama vrijednosti a, b i c.
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 4;
+		int c= 10;
+		
+		if (a + b > c){
+			System.out.println("Moguce je konstruisati trougao.");
+		} else {
+			System.out.println("Nemoguce je konstruisati trougao.");
+		}
+			
+
+	}
+
+}
